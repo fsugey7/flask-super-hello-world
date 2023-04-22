0fb1c1739b24242b77560c1627868b641ddb795d
sugey.sugey <sugey,flores@soymas.cl>
2023-04-21 22:58:24 -0400

    cambio

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index_world():
     return render_template('index.html')
 @app.route('/welcome')
 def welcome_world():
