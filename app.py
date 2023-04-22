0d25f7b43e44dc5b2df37f65ea77833f8d23f140
sugey.sugey <sugey,flores@soymas.cl>
2023-04-21 22:55:13 -0400

    actividad1

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+@app.route('/welcome')
+def welcome_world():
+    return render_template('welcome.html')
