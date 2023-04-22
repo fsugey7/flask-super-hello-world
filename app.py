856f9abdcbd745c3877daaf7c355d13b729e8512
sugey.sugey <sugey,flores@soymas.cl>
2023-04-21 23:06:24 -0400

    tarea

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 @app.route('/welcome')
-def welcome_world():
-    return render_template('welcome.html')
+def welcome():
+    return render_template('welcome.html', user = request.args.get('username'))
